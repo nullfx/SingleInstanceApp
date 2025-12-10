a278bb3dddf83b814daa9ea16ab4d7f5c6540648
nullfx <steve@nullfx.com>
2025-12-09 23:17:47 -0800

    initial sample commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SingleInstanceApp
-.net single instance app using mutex and win32
+# Single instance app example
+
+This is a sample C# .net windows forms app which employs a mutex and win32 to prevent multiple instances of itself from running and brings the original instance to the foreground when subsequent instances run.
+
+
+
+![](C:\repos\SingleInstanceApp\images\single-instance-app.png)
---
+++ src/SingleInstanceApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Forms;
+
+namespace SingleInstanceApp {
+public partial class Form1 : Form {
+    public Form1 ( ) {
+        InitializeComponent ( );
+    }
+
+    protected override void WndProc ( ref Message m ) {
+
+        // handle the WM_SHOWME message and bring 
+        // this window to the top
+        if ( m.Msg == NativeMethods.WM_SHOWME ) {
+            ShowMe ( );
+        }
+        base.WndProc ( ref m );
+    }
+
+    private void ShowMe ( ) {
+        if ( WindowState == FormWindowState.Minimized ) {
+            WindowState = FormWindowState.Normal;
+        }
+        // get our current "TopMost" value (ours will always be false though)
+        bool top = TopMost;
+        // make our form jump to the top of everything
+        TopMost = true;
+        // set it back to whatever it was
+        TopMost = top;
+    }
+
+        private void HandleExitClick ( object sender, System.EventArgs e ) {
+            Close ();
+        }
+    }
+}
